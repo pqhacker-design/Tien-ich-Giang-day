1d5eeb26562c0d1d43a75d91b6789ca2c1cabe93
pqhacker-design <pqhacker@gmail.com>
2026-06-18 10:37:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ with col3:
 
 with col4:
     with st.container(border=True):
-        st.markdown("### 📈 4. Máy tính Lãi suất kép")
+        st.markdown("### 📝 4. Sửa lỗi chính tả và ngữ pháp Tiếng Việt")
         st.write("Ứng dụng Kiểm tra và Sửa lỗi Chính tả/Ngữ pháp Tiếng Việt chuyên sâu dành cho file Word (.docx)")
         if st.button("🚀 Mở ứng dụng Máy tính Lãi suất", key="btn_lai_suat", use_container_width=True):
             st.switch_page("vietnamese-word-corrector/4_📝_Sửa_lỗi_chính_tả.py")
