3fd8cf986039b10beaa09109ef822db9ae2b32c3
pqhacker-design <pqhacker@gmail.com>
2026-06-20 15:08:31 +0700

    Update 6_🎮_Trò_chơi_học_tập.py

--- pages/6_🎮_Trò_chơi_học_tập.py
+++ pages/6_🎮_Trò_chơi_học_tập.py
@@ -36,7 +36,6 @@ else:
 openai_key = st.session_state.get("openai_api_key", "").strip()
 
 with st.sidebar:
-    s# Nếu lưu ảnh về máy/server
     st.image("assets/brainstorm-skill.png", width=80)
     st.title("⚙️ CẤU HÌNH HOẠT ĐỘNG")
     st.markdown("---")
