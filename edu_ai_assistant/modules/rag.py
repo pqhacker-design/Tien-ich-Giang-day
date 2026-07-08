4c2dbb062ac31601460f14f652011e2badd36a22
pqhacker-design <pqhacker@gmail.com>
2026-07-08 14:51:27 +0700

    Update rag.py

--- edu_ai_assistant/modules/rag.py
+++ edu_ai_assistant/modules/rag.py
@@ -1,7 +1,7 @@
 import os
 from langchain_community.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 import streamlit as st
 
